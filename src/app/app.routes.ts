4810bfc4fb91c157defca0dbb750c68309d61cbd
AnuragKumarChoudhary <anuragkumarchoudhary@outlook.com>
2024-05-24 20:10:32 +0530

    Initial Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { HomComponent } from './hom/hom.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomComponent }
+];
